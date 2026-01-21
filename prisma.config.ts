637039d134e092500e8c1585c920914a6e709a78
Fahmi Alkatiry <emailgithubkamu@gmail.com>
2026-01-21 17:25:00 +0700

    Initial commit

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   datasource: {
+    // Prisma 7 akan membaca URL dari sini untuk perintah 'db push' dan 'migrate'
     url: process.env.DATABASE_URL,
   },
 });
