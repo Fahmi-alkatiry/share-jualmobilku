4c9ce28b8b0785ea2908a3fcd995ff9557c6768a
Fahmi Alkatiry <emailgithubkamu@gmail.com>
2026-01-21 18:13:30 +0700

    awal

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,12 +4,6 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+  new PrismaClient() // Polos saja, Prisma akan otomatis baca dari .env
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
