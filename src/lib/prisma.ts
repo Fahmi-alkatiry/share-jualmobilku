8f0609436ede79927c35d42a89e4953f78514bb5
Fahmi Alkatiry <emailgithubkamu@gmail.com>
2026-01-21 17:59:19 +0700

    awal

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// Gunakan connection string dari .env
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-const adapter = new PrismaPg(pool)
-
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({ adapter }) // Adapter digunakan di sini
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
