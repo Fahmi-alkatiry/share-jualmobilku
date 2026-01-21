6f7a84aa9cf64ff8a8933b80bb646dda28107a6b
Fahmi Alkatiry <emailgithubkamu@gmail.com>
2026-01-21 17:38:32 +0700

    Initial commit

--- src/auth.ts
+++ src/auth.ts
@@ -2,26 +2,27 @@ import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    Credentials({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        // Validasi dengan data di .env
-        if (
-          credentials?.username === process.env.ADMIN_USERNAME &&
-          credentials?.password === process.env.ADMIN_PASSWORD
-        ) {
-          return { id: "1", name: "Admin JualMobilKu" }
-        }
-        return null
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login", // Arahkan ke halaman login custom kita
-  },
+    trustHost: true,
+    providers: [
+        Credentials({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                // Validasi dengan data di .env
+                if (
+                    credentials?.username === process.env.ADMIN_USERNAME &&
+                    credentials?.password === process.env.ADMIN_PASSWORD
+                ) {
+                    return { id: "1", name: "Admin JualMobilKu" }
+                }
+                return null
+            },
+        }),
+    ],
+    pages: {
+        signIn: "/login", // Arahkan ke halaman login custom kita
+    },
 })
